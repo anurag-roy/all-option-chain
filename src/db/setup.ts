3f991c9ad369f728f7503dd0d5b0587b8d656e9d
Anurag Roy <anurag@2pirad.com>
2023-09-12 09:54:55 +0530

    Setup DB

---
+++ src/db/setup.ts
@@ -0,0 +1,75 @@
+import { getKeys } from '@/lib/utils';
+import Database from 'better-sqlite3';
+import { getInstruments } from './getInstruments';
+
+// Create the sqlite db here
+const DB_PATH = 'src/db/data.db';
+
+const getSqliteType = (key: string, value: any) =>
+  typeof value === 'number' ? 'REAL' : 'TEXT';
+
+async function main() {
+  const nseInstruments = await getInstruments('NSE');
+  const nfoInstruments = await getInstruments('NFO');
+
+  const instruments = [...nseInstruments, ...nfoInstruments];
+
+  const columns = getKeys(instruments[0]);
+
+  const TABLE_NAME = 'instrument';
+  const INSERT_BATCH_SIZE = 10000;
+
+  // Create DB and table
+  const db = new Database(DB_PATH);
+  console.log('DB creation successful!');
+
+  db.prepare(
+    `CREATE TABLE ${TABLE_NAME} (` +
+      'id TEXT NOT NULL PRIMARY KEY,' +
+      columns.map(
+        (c) => `${c} ${getSqliteType(c, instruments[0][c])} NOT NULL`
+      ) +
+      ');'
+  ).run();
+  console.log('Table creation successful!');
+
+  const insert = (values: string[]) => {
+    if (values.length === 0) return;
+    db.exec(
+      `INSERT INTO ${TABLE_NAME} (id, ${columns.join(
+        ','
+      )}) VALUES ${values.join(',')};`
+    );
+  };
+
+  // Variables to insert values in batches
+  let currentBatchValues = [];
+  let currentIteration = 0;
+
+  console.log('Starting insert...');
+  for (const instrument of instruments) {
+    const currentRowValues = [];
+
+    // Insert primary key Id
+    currentRowValues.push(`'${instrument.tradingSymbol}'`);
+
+    for (const col of columns) {
+      currentRowValues.push(`'${instrument[col]}'`);
+    }
+    currentBatchValues.push(`(${currentRowValues.join(',')})`);
+
+    // Execute insert statement once a batch is full
+    currentIteration++;
+    if (currentIteration % INSERT_BATCH_SIZE === 0) {
+      insert(currentBatchValues);
+      currentBatchValues = [];
+    }
+  }
+  // Fire once more for leftover values
+  insert(currentBatchValues);
+  console.log('Data insertion successful!');
+}
+
+main().catch((error) => {
+  console.log('Data preparation failed', error);
+});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,46 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export const getKeys = <T extends Object>(object: T) =>
+  Object.keys(object) as Array<keyof T>;
+
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const getMonthName = (monthIndex: number) => {
+  // Adjust from 0 based month to irl month index
+  const numericMonth = (monthIndex + 1).toString();
+  const monthDate = new Date(numericMonth);
+  return Intl.DateTimeFormat('en', { month: 'short' })
+    .format(monthDate)
+    .toUpperCase();
+};
+
+export const getExpiryOptions = (optionCount: number) => {
+  const date = new Date();
+  let currentMonthIndex = date.getMonth();
+  let currentYear = date.getFullYear();
+
+  const options: string[] = [];
+
+  for (let i = 0; i < optionCount; i++) {
+    options.push(`${getMonthName(currentMonthIndex)}-${currentYear}`);
+
+    currentMonthIndex = currentMonthIndex + 1;
+    if (currentMonthIndex > 11) {
+      currentMonthIndex = 0;
+      currentYear = currentYear + 1;
+    }
+  }
+
+  return options;
+};
+
+export const displayInr = (amount: number) =>
+  '₹ ' +
+  new Intl.NumberFormat('en-IN', {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+  }).format(amount);
+
+export const getRandomIndex = (min: number, max: number) =>
+  Math.random() * (max - min) + min;

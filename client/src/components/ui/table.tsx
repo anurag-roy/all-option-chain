d71889509ddb380ab45a424b1d14c762fb240fe5
Anurag Roy <anuragroy@duck.com>
2026-06-29 10:17:39 +0530

    Fix class lints

--- client/src/components/ui/table.tsx
+++ client/src/components/ui/table.tsx
@@ -48,7 +48,7 @@ function TableHead({ className, ...props }: React.ComponentProps<'th'>) {
     <th
       data-slot='table-head'
       className={cn(
-        'text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0',
+        'text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap has-[[role=checkbox]]:pr-0',
         className
       )}
       {...props}
@@ -60,7 +60,7 @@ function TableCell({ className, ...props }: React.ComponentProps<'td'>) {
   return (
     <td
       data-slot='table-cell'
-      className={cn('p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0', className)}
+      className={cn('p-2 align-middle whitespace-nowrap has-[[role=checkbox]]:pr-0', className)}
       {...props}
     />
   );
